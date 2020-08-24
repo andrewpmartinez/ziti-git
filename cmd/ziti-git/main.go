0ab76789a7ca2c03ad415f6476d924579be7a912
Andrew Martinez <andrew.p.martinez@gmail.com>
2020-08-24 10:35:07 -0400

    make checkout relative to parent dir

--- cmd/ziti-git/main.go
+++ cmd/ziti-git/main.go
@@ -220,10 +220,19 @@ func init() {
 			if err != nil {
 				formattedErrorExit("could not get go mod info for repo [%s]: %v", repoDir, err)
 			}
+
+			parentPath := repoDir + string(filepath.Separator) + ".."
+			absParentPath, err := filepath.Abs(parentPath)
+
+			if err != nil {
+				formattedErrorExit("cannot determine parent path for [%s]: %v", parentPath, err)
+			}
+
 			script := "\n"
+			script += fmt.Sprintf(`cd "%s"`, absParentPath) + "\n"
 			for _, require := range info.Require {
 				if dirName, found := ZitiModules[require.Mod.Path]; found {
-					script += fmt.Sprintf(`git checkout -C "%s" %s`, "../"+dirName, require.Mod.Version) + "\n"
+					script += fmt.Sprintf(`git -C "%s" checkout %s`, "./"+dirName, require.Mod.Version) + "\n"
 				}
 			}
 
